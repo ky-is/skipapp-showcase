17b4f588f2dc9246ed13f30ff0732a0c90972bd5
kyle <kylecoburn@gmail.com>
2024-12-26 18:16:49 -0800

    Add page TabView style cases
    Requires https://github.com/skiptools/skip-ui/pull/95

--- Sources/Showcase/TabViewPlayground.swift
+++ Sources/Showcase/TabViewPlayground.swift
@@ -16,13 +16,95 @@ struct TabViewPlayground: View {
             TabPlaygroundContentView(label: "Favorites", selectedTab: $selectedTab)
                 .tabItem { Label("Favorites", systemImage: "heart.fill") }
                 .tag("Favorites")
-            TabPlaygroundContentView(label: "Info", selectedTab: $selectedTab)
-                .tabItem { Label("Info", systemImage: "info.circle.fill") }
-                .tag("Info")
+            TabPageViewContentView()
+                .tabItem { Label("Paging", systemImage: "arrow.forward.square") }
+                .tag("Paging")
         }
         .tint(.red)
         .toolbar {
             PlaygroundSourceLink(file: "TabViewPlayground.swift")
+        }
+    }
+
+    struct TabPageViewContentView: View {
+        @State var showExtraPage = false
+
+        var body: some View {
+            ScrollView {
+                VStack {
+                    TabView {
+                        Rectangle()
+                            .fill(Color.gray)
+                            .overlay {
+                                Text("This is a horizontally swipable paging TabView")
+                            }
+                        Text("2")
+                            .background(Color.red)
+                        Text("I heard you like TabViews so we put a TabView inside your TabView inside your TabView")
+                            .background(Color.green)
+                    }
+                    .tabViewStyle(.page)
+                    .frame(height: 128)
+                    TabView {
+                        Rectangle()
+                            .fill(Color.purple)
+                            .frame(width: 256, height: 64)
+                            .overlay {
+                                Text("Fixed size content, flexible container")
+                            }
+                        Rectangle()
+                            .fill(Color.purple)
+                            .frame(width: 64, height: 96)
+                    }
+                    .tabViewStyle(.page)
+                    TabView {
+                        Image(systemName: "heart")
+                            .resizable()
+                            .background(Color.pink)
+                        Image(systemName: "heart.fill")
+                            .resizable()
+                            .background(Color.red)
+                    }
+                    .tabViewStyle(.page)
+                    .aspectRatio(2, contentMode: ContentMode.fill)
+                    TabView {
+                        Rectangle()
+                            .fill(Color.blue)
+                            .overlay {
+                                Button("Tap to \(showExtraPage ? "hide" : "show") extra page") {
+                                    showExtraPage = !showExtraPage
+                                }
+                            }
+                        if showExtraPage {
+                            Button("Hide this page") {
+                                showExtraPage = false
+                            }
+                        }
+                    }
+                    .tabViewStyle(.page)
+                    .frame(height: 128)
+                    TabView {
+                        Rectangle()
+                            .fill(Color.mint)
+                            .overlay {
+                                Text("Single page with indicator showing")
+                            }
+                    }
+                    .tabViewStyle(.page(indexDisplayMode: .always))
+                    .frame(height: 128)
+                    TabView {
+                        Rectangle()
+                            .fill(Color.green)
+                            .overlay {
+                                Text("Multi page with indicator hidden")
+                            }
+                        Text("2")
+                    }
+                    .tabViewStyle(.page(indexDisplayMode: .never))
+                    .frame(height: 128)
+                }
+                .foregroundStyle(Color.white)
+            }
         }
     }
 }
@@ -44,11 +126,15 @@ struct TabPlaygroundContentView: View {
                     selectedTab = "Favorites"
                 }
             }
-            if label != "Info" {
-                Button("Switch to Info") {
-                    selectedTab = "Info"
+            if label != "Paging" {
+                Button("Switch to Paging") {
+                    selectedTab = "Paging"
                 }
             }
         }
     }
+}
+
+#Preview {
+    TabViewPlayground()
 }
